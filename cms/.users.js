b04c41861dc8fc5d07095452f5dd2d55833610a7
Oriol Torrent <oriol.tf@me.com>
2017-05-16 13:10:32 +0200

    Add admin user `rosajp`

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'rosajp',
+			tags: [],
+			salt: '0de84d2d0cf3c97b65a6711bbf42963d',
+			hash: '6ed0d65f24c8902e2113f804ca714af945e4579028049e2918648a36138b0c63',
+			user_created_timestamp: 1494932997602
+		}
+	]
+}
